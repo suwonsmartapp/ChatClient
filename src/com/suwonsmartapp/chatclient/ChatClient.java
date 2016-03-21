f24c821c8bb75a4a3f7955a2d49b84958b2c53db
donghae.choi <ddo921124@gmail.com>
2016-03-21 18:51:56 +0900

    Json을 이용한 메세지 주고받기 1. 메세지를 정상적으로 받지 못하던 오류 수정

--- src/com/suwonsmartapp/chatclient/ChatClient.java
+++ src/com/suwonsmartapp/chatclient/ChatClient.java
@@ -1,115 +1,111 @@
 package com.suwonsmartapp.chatclient;
 
+import com.google.gson.Gson;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 public class ChatClient {
-//	private final static String SERVER_HOST = "192.168.0.222";
-	private final static String SERVER_HOST = "localhost";
+	private final static String SERVER_HOST = "192.168.0.239";
+	//	private final static String SERVER_HOST = "localhost";
 	private final static int SERVER_PORT = 5000;
-	
+
 	private Socket mSocket;
-	
+	private MsgInfo msgInfo;
+	private String mName;
+
 	public static void main(String[] args) {
 		new ChatClient().connect();
 	}
-	
+
 	public void connect() {
 		try {
 			mSocket = new Socket(SERVER_HOST, SERVER_PORT);
-			
-			new ClientReciver(mSocket, "닉네임").start();
-			
+			ClientWrite clientWrite = new ClientWrite("최동해");
+			clientWrite.start();
+			ClientRead clientRead = new ClientRead();
+			clientRead.start();
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	class ClientWriter extends Thread {
-		
+	class ClientRead extends Thread {
+		private DataInputStream mInputStream;
+
 		@Override
 		public void run() {
-			// TODO Auto-generated method stub
+			try {
+				mInputStream = new DataInputStream(mSocket.getInputStream());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 			super.run();
+			try {
+				// 계속 듣기만
+				while (mInputStream != null) {
+					// json 파싱
+					String json = mInputStream.readUTF();
+					try {
+						MsgInfo msgInfo = new Gson().fromJson(json, MsgInfo.class);
+						System.out.println(msgInfo);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				// 접속 종료시
+				mSocket = null;
+			}
 		}
 	}
-	
-	class ClientReciver extends Thread {
-		
-		private DataInputStream mInputStream;
+
+	class ClientWrite extends Thread {
+
 		private DataOutputStream mOutputStream;
-		
-		public ClientReciver(Socket socket, String nickName) {
+
+		public ClientWrite(String nickName) {
 			try {
-				mInputStream = new DataInputStream(socket.getInputStream());
-				mOutputStream = new DataOutputStream(socket.getOutputStream());
-				
+				mName = nickName;
+				mOutputStream = new DataOutputStream(mSocket.getOutputStream());
 				mOutputStream.writeUTF(nickName);
 				mOutputStream.flush();
-
 				System.out.println("id : " + nickName + "접속 완료");
-				
-				while (true) {
-					System.out.print("메세지 입력 : ");
-					Scanner in = new Scanner(System.in);
-					
-					// Json구성
-					String msg = in.nextLine();
-					long time = System.currentTimeMillis();
-					
-					MsgInfo msgInfo = new MsgInfo(nickName, msg, time);
-					Gson gson = new Gson();
-					
-					
-//					String json = "{\"nickName\":\"" + nickName + "\",\"msg\":\"" + msg + "\",\"time\":\"" + time + "\"}";
-					
-					mOutputStream.writeUTF(gson.toJson(msgInfo));
-					mOutputStream.flush();
-				}
-				
-//				try {
-//					Thread.sleep(5000);
-//					mOutputStream.writeUTF("exit");
-//					mOutputStream.flush();
-//					System.out.println("id : " + nickName + "접속 종료");
-//					System.exit(0);
-//				} catch (InterruptedException e) {
-//				}
-				
 			} catch (IOException e) {
 				e.printStackTrace();
 				System.out.println("writeUTF IOException");
 			}
 		}
-		
+
 		@Override
 		public void run() {
-			try {
-				// 계속 듣기만
-				while (mInputStream != null) {
-					// json 파싱
-					Gson gson = new Gson();
-					String json = mInputStream.readUTF();
-					MsgInfo msgInfo = gson.fromJson(json, MsgInfo.class); 
-					System.out.println(msgInfo.toString());
+			Scanner in = new Scanner(System.in);
+
+			while (true) {
+				System.out.print("메세지 입력 : ");
+				// Json구성
+				String msg = in.nextLine();
+				long time = System.currentTimeMillis();
+				msgInfo = new MsgInfo(mName, msg, time);
+
+				Gson gson = new Gson();
+//					String json = "{\"nickName\":\"" + nickName + "\",\"msg\":\"" + msg + "\",\"time\":\"" + time + "\"}";
+				try {
+					mOutputStream.writeUTF(gson.toJson(msgInfo));
+//                    System.out.println(gson.toJson(msgInfo));
+					mOutputStream.flush();
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			} finally {
-				// 접속 종료시	
-				mSocket = null;
-				
-			}	
+
+			}
 		}
 	}
 }
