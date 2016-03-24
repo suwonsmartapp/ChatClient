ea8c4ba428a463d6e3fade0ac8ff6421fcee425c
Junsuk Oh <a811219@gmail.com>
2016-03-24 09:51:59 +0900

    리펙토링

--- src/com/suwonsmartapp/chatclient/ChatClient.java
+++ src/com/suwonsmartapp/chatclient/ChatClient.java
@@ -10,102 +10,102 @@ import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class ChatClient {
-	private final static String SERVER_HOST = "192.168.0.239";
-	//	private final static String SERVER_HOST = "localhost";
-	private final static int SERVER_PORT = 5000;
+    private final static String SERVER_HOST = "localhost";
+    private final static int SERVER_PORT = 5000;
+    private final static String NICKNAME = "닉네임";
 
-	private Socket mSocket;
-	private MsgInfo msgInfo;
-	private String mName;
+    private Socket mSocket;
+    private String mName;
 
-	public static void main(String[] args) {
-		new ChatClient().connect();
-	}
+    public static void main(String[] args) {
+        new ChatClient().connect();
+    }
 
-	public void connect() {
-		try {
-			mSocket = new Socket(SERVER_HOST, SERVER_PORT);
-			ClientWrite clientWrite = new ClientWrite("최동해");
-			clientWrite.start();
-			ClientRead clientRead = new ClientRead();
-			clientRead.start();
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	class ClientRead extends Thread {
-		private DataInputStream mInputStream;
+    public void connect() {
+        try {
+            mSocket = new Socket(SERVER_HOST, SERVER_PORT);
+            ClientWrite clientWrite = new ClientWrite(NICKNAME);
+            clientWrite.start();
+            ClientRead clientRead = new ClientRead();
+            clientRead.start();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-		@Override
-		public void run() {
-			try {
-				mInputStream = new DataInputStream(mSocket.getInputStream());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			super.run();
-			try {
-				// 계속 듣기만
-				while (mInputStream != null) {
-					// json 파싱
-					String json = mInputStream.readUTF();
-					try {
-						MsgInfo msgInfo = new Gson().fromJson(json, MsgInfo.class);
-						System.out.println(msgInfo);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			} finally {
-				// 접속 종료시
-				mSocket = null;
-			}
-		}
-	}
+    class ClientRead extends Thread {
+        private DataInputStream mInputStream;
 
-	class ClientWrite extends Thread {
+        @Override
+        public void run() {
+            try {
+                mInputStream = new DataInputStream(mSocket.getInputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            super.run();
+            try {
+                // 계속 듣기만
+                while (mInputStream != null) {
+                    // json 파싱
+                    String json = mInputStream.readUTF();
+                    try {
+                        MsgInfo msgInfo = new Gson().fromJson(json, MsgInfo.class);
+                        System.out.println(msgInfo);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                // 접속 종료시
+                mSocket = null;
+            }
+        }
+    }
 
-		private DataOutputStream mOutputStream;
+    class ClientWrite extends Thread {
 
-		public ClientWrite(String nickName) {
-			try {
-				mName = nickName;
-				mOutputStream = new DataOutputStream(mSocket.getOutputStream());
-				mOutputStream.writeUTF(nickName);
-				mOutputStream.flush();
-				System.out.println("id : " + nickName + "접속 완료");
-			} catch (IOException e) {
-				e.printStackTrace();
-				System.out.println("writeUTF IOException");
-			}
-		}
+        private DataOutputStream mOutputStream;
 
-		@Override
-		public void run() {
-			Scanner in = new Scanner(System.in);
+        public ClientWrite(String nickName) {
+            try {
+                mName = nickName;
+                mOutputStream = new DataOutputStream(mSocket.getOutputStream());
+                mOutputStream.writeUTF(nickName);
+                mOutputStream.flush();
+                System.out.println("id : " + nickName + "접속 완료");
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("writeUTF IOException");
+            }
+        }
 
-			while (true) {
-				System.out.print("메세지 입력 : ");
-				// Json구성
-				String msg = in.nextLine();
-				long time = System.currentTimeMillis();
-				msgInfo = new MsgInfo(mName, msg, time);
+        @Override
+        public void run() {
+            Scanner in = new Scanner(System.in);
 
-				Gson gson = new Gson();
+            while (true) {
+                System.out.print("메세지 입력 : ");
+                // Json구성
+                String msg = in.nextLine();
+                long time = System.currentTimeMillis();
+                MsgInfo msgInfo = new MsgInfo(mName, msg, time);
+
+                Gson gson = new Gson();
 //					String json = "{\"nickName\":\"" + nickName + "\",\"msg\":\"" + msg + "\",\"time\":\"" + time + "\"}";
-				try {
-					mOutputStream.writeUTF(gson.toJson(msgInfo));
+                try {
+                    mOutputStream.writeUTF(gson.toJson(msgInfo));
 //                    System.out.println(gson.toJson(msgInfo));
-					mOutputStream.flush();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+                    mOutputStream.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-			}
-		}
-	}
+            }
+        }
+    }
 }
